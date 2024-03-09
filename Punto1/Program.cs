12e58b93ca8721237f33059af4ecae9e65c1440e
cflorian <camiloflorian14@gmail.com>
2024-03-08 20:35:09 -0500

    Variable

---
+++ Punto1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Punto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un sueldo base de $XXXX.Además, recibe una comisión del 10 % sobre el valor total de cada venta querealiza.El vendedor desea saber
+
+            //Declaracion de Variables
+            double sueldobase = 800000;
+            double venta1, venta2, venta3;
+            double totalcomisiones;
+            double mayorcomision;
+        }
+    }
+}
