1cf29b5a9940d6b1d7167af37f7fcfa59cd4ee3c
cflorian <camiloflorian14@gmail.com>
2024-03-08 21:55:50 -0500

    calculos

--- SegundoPunto/Program.cs
+++ SegundoPunto/Program.cs
@@ -12,6 +12,14 @@ namespace SegundoPunto
             int horasalidatren1 = 10;
             int horasalidatren2 = 11;
 
+            //Calculo del tiempo transcurrido entre la salida del primer y segundo tren
+            double tiempotranscurrido = horasalidatren2 - horasalidatren1;
+
+            //Calculo de la distancia recorrida por el primer tren durante este tiempo en KM
+            double distanciarecorridatren1 = velocidadtren1 * velocidadtren2;
+
+
+
 
 
 
