83dd16254885bdac275b5a65d3cd6bc5a24afc23
cflorian <camiloflorian14@gmail.com>
2024-03-08 21:49:10 -0500

    Declaracion de variable

---
+++ SegundoPunto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SegundoPunto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variable
+            int velocidadtren1;
+            int velocidadtren2;
+            int horasalidatren1;   
+            int horasalidatren2;
+            
+
+        }
+    }
+}
